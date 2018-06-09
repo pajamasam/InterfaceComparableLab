3341afbc50e137eadeef3db34faa8de8a3a533f5
Nicholas Maidanos <nmaidanos@gmail.com>
2018-06-09 10:28:34 -0400

    added the Paypal/Paypal test classes

--- src/main/java/com/zipcoder/payment/Paypal.java
+++ src/main/java/com/zipcoder/payment/Paypal.java
@@ -5,7 +5,47 @@ public class Paypal implements Payment {
     private long id;
     private String payerName;
     private String email;
+
     public Paypal(){};
+
+    public Paypal(long id, String payerName, String email){
+        this.id = id;
+        this.payerName = payerName;
+        this.email = email;
+    }
+
+    public void setId(long id){
+        this.id = id;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setPayerName(String payerName){
+        this.payerName = payerName;
+    }
+
+    public String getPayerName() {
+        return this.payerName;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+
+    public String getShortDescription() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("PayPal ");
+        sb.append(this.payerName + " ");
+        sb.append(this.email);
+        return sb.toString();
+    }
 
 
 }
---
+++ src/test/java/com/zipcoder/payment/PaypalTest.java
@@ -0,0 +1,107 @@
+package com.zipcoder.payment;
+
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.Assert.assertEquals;
+
+class PaypalTest {
+
+    @Test
+    void setId() {
+        //When
+        Paypal pp = new Paypal();
+        pp.setId(75);
+
+        //Expect
+        long expect = 75;
+
+        //Actual
+        long actual = pp.getId();
+        assertEquals(expect, actual);
+    }
+
+    @Test
+    void getId() {
+        //When
+        Paypal pp = new Paypal();
+        pp.setId(75);
+
+        //Expect
+        long expect = 75;
+
+        //Actual
+        long actual = pp.getId();
+        assertEquals(expect, actual);
+    }
+
+    @Test
+    void setPayerName() {
+        //When
+        Paypal pp =  new Paypal();
+        pp.setPayerName("Nicholas Maidanos");
+
+        //Expect
+        String expect = "Nicholas Maidanos";
+
+        //Actual
+        String actual = pp.getPayerName();
+        assertEquals(expect, actual);
+    }
+
+    @Test
+    void getPayerName() {
+        //When
+        Paypal pp =  new Paypal();
+        pp.setPayerName("Nicholas Maidanos");
+
+        //Expect
+        String expect = "Nicholas Maidanos";
+
+        //Actual
+        String actual = pp.getPayerName();
+        assertEquals(expect, actual);
+    }
+
+    @Test
+    void setEmail() {
+        //When
+        Paypal pp = new Paypal();
+        pp.setEmail("example@email.com");
+
+        //Expect
+        String expect = "example@email.com";
+
+        //Actual
+        String actual = pp.getEmail();
+        assertEquals(expect, actual);
+    }
+
+    @Test
+    void getEmail() {
+        //When
+        Paypal pp = new Paypal();
+        pp.setEmail("example@email.com");
+
+        //Expect
+        String expect = "example@email.com";
+
+        //Actual
+        String actual = pp.getEmail();
+        assertEquals(expect, actual);
+    }
+
+    @Test
+    void getShortDescription() {
+        //When
+        Paypal pp = new Paypal(78, "Nicholas Madianos", "nmaidanos@gmail.com");
+
+        //Expect
+        String expect = "PayPal Nicholas Madianos nmaidanos@gmail.com";
+
+        //Actual
+        String actual = pp.getShortDescription();
+
+        assertEquals(expect,actual);
+    }
+}
